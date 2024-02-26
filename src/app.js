3c5411356c17ded0176261bda92f1569fd07484f
Harshit Soni <kratin67soni@gmail.com>
2024-02-26 11:58:23 +0530

    added subscrition controller and router

--- src/app.js
+++ src/app.js
@@ -23,6 +23,8 @@ import videoRouter from "./routes/video.routes.js";
 import commentRouter from "./routes/comment.routes.js";
 import dashboardRouter from "./routes/dashboard.routes.js";
 import healthcheckRouter from "./controllers/healthcheck.controller.js";
+import playlistRouter from "./routes/playlist.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
 
 //routes declaration
 app.use("/api/v1/users", userRouter);
@@ -30,5 +32,7 @@ app.use("/api/v1/videos", videoRouter);
 app.use("/api/v1/comments", commentRouter);
 app.use("/api/v1/dashboard", dashboardRouter);
 app.use("/api/v1/healthcheck", healthcheckRouter);
+app.use("/api/v1/playlist", playlistRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
 
 export { app };
---
+++ src/controllers/subscription.controller.js
@@ -0,0 +1,97 @@
+import mongoose, { isValidObjectId } from "mongoose";
+import { User } from "../models/user.model.js";
+import { Subscription } from "../models/subscription.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+const toggleSubscription = asyncHandler(async (req, res) => {
+  const { channelId } = req.params;
+  // TODO: toggle subscription
+});
+
+// controller to return subscriber list of a channel
+const getUserChannelSubscribers = asyncHandler(async (req, res) => {
+  const { channelId } = req.params;
+
+  const subscribersList = await User.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(channelId),
+      },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "channel",
+        as: "subscriptions",
+        pipeline: [
+          {
+            $lookup: {
+              from: "users",
+              localField: "subscriber",
+              foreignField: "_id",
+              as: "subscribers",
+              pipeline: [
+                {
+                  $project: {
+                    fullName: 1,
+                    avatar: 1,
+                    username: 1,
+                  },
+                },
+              ],
+            },
+          },
+        ],
+      },
+    },
+  ]);
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        subscribersList[0].Subscriptions[0].subsribers,
+        "Channel Subscribers fetched successfully"
+      )
+    );
+});
+
+// controller to return channel list to which user has subscribed
+const getSubscribedChannels = asyncHandler(async (req, res) => {
+  const { subscriberId } = req.params;
+
+  const channelsSubscribedTo = await User.aggregate([
+    {
+      $match: {
+        _id: new mongoose.Types.ObjectId(subscriberId),
+      },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "Subscriber",
+        as: "channelsSubscribed",
+      },
+    },
+    {
+      $project: {
+        channelsSubscribed: 1,
+      },
+    },
+  ]);
+
+  return res
+    .status(200)
+    .json(
+      200,
+      channelsSubscribedTo[0],
+      "Channels subscribed to fetched successfully"
+    );
+});
+
+export { toggleSubscription, getUserChannelSubscribers, getSubscribedChannels };
